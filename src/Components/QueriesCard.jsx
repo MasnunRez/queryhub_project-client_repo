2bde76c0f7cb9de18f354ca8579bf5637a422d52
MasnunRez <masnun.rezon93@gmail.com>
2025-08-17 04:54:17 +0600

    Card design updated

--- src/Components/QueriesCard.jsx
+++ src/Components/QueriesCard.jsx
@@ -5,25 +5,26 @@ const QueriesCard = ({query, cols}) => {
   const {productImageUrl, queryTitle, boycottingReason, productName}= query.queryData
   const {recommendationCount}= query
 
-  const isThreeCols = cols === 1;
+  const isOneCols = cols === 1;
 
     return (
-        <div className={`p-4 bg-gray-100 rounded-2xl my-4
+        <div className={` p-4 bg-gray-50 border border-gray-200 rounded-2xl my-4
         ${
-          isThreeCols ? "flex gap-5" : "md:flex-row"
+          isOneCols ? "flex gap-5" : "flex flex-col gap-5"
         }`}>
 
           <div className="">
-            <img className='w-[200px]' src={productImageUrl} alt="" />
+            <img className='w-[300px]' src={productImageUrl} alt="" />
           </div>
+          {/* Text area  */}
           <div className="">
-            <h3 className='text-2xl font-bold mb-4'>{queryTitle}</h3>
+            <h3 className=' sm:text-2xl font-bold mb-4'>{queryTitle}</h3>
             <p>Product: {productName}</p>
             <p>Details: {boycottingReason}</p>
             <div className="space-x-3 mt-4">
               <p>Total Recommendation: {recommendationCount} </p>
             </div>
-            <div className="mt-auto pt-4">
+            <div className="">
               <Link to={`/querydetails/${query._id}`}><button className="text-green-600 font-bold hover:bg-green-200 border px-4 rounded-2xl mt-2">Recommend</button></Link>
 
             </div>
